696da1b8091aaccc1f1925152dbc5c6493ab96a6
Jalen Wu <jalenwu@Jalens-MacBook-Air.local>
2024-02-03 16:36:20 -0500

    added current location component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import { Button } from 'react-bootstrap'
+import React from "react";
 import yes from './images/yes.png';
 import no from './images/no.png';
 
 import './App.css';
+import CurrentLocation from "./CurrentLocation.js";
 
 /** To represent the main block of code running our web application */
 function App() {
@@ -17,19 +17,20 @@ function App() {
          
         </h2>
       </header>
-      <div className='landing-page'>
-        <h2>
-          Welcome to the project web application!
-        </h2>
-        <p>
-          This will be the landing page for the project web application.
-        </p>
-        <div className="yes-no-bar">
-          <h3>[include location entry here]</h3>
-          <img src={yes}className="option" alt="yes" onClick={() => alert("Clicked yes")}/>
-          <img src={no} className="option" alt="no" onClick={() => alert("Clicked no")} />
-        </div>
-      </div>
+            <div className='landing-page'>
+              <h2>
+                Welcome to the project web application!
+              </h2>
+              <p>
+                This will be the landing page for the project web application.
+              </p>
+              <CurrentLocation />
+              <div className="yes-no-bar">
+                <h3>[include location entry here]</h3>
+                <img src={yes}className="option" alt="yes" onClick={() => alert("Clicked yes")}/>
+                <img src={no} className="option" alt="no" onClick={() => alert("Clicked no")} />
+              </div>
+            </div>
     </div>
   );
 }
---
+++ src/CurrentLocation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CurrentLocation = () => {
+    return (
+        <div>
+            <h2>Current Location</h2>
+        </div>
+    );
+}
+
+export default CurrentLocation;
