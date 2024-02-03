6c65e9d3c5a2d85f34b7cbbd3a341b7c8fd330c1
Jalen Wu <jalenwu@Jalens-MacBook-Air.local>
2024-02-03 13:17:59 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Brown Hackathon 2024
+        </h1>
+      </header>
+      <div className='landing-page'>
+        <h2>
+          Welcome to the project web application!
+        </h2>
+        <p>
+          This will be the landing page for the project web application.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
