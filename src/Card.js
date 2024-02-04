5af71713ebb85db3043c4b8a89729053d92aca47
Jalen Wu <jalenwu@Jalens-Air.fios-router.home>
2024-02-04 02:02:39 -0500

    final commit tonight

--- src/Card.js
+++ src/Card.js
@@ -11,13 +11,13 @@ const Card = () => {
     console.log(locs[0].rating)
     return (
     <div className="card">
-        <h1>Location Title</h1>
-        <br></br>
-        <p>Location Image</p>
-        <br></br>
-        <br></br>
-        <p>Card Description</p>
-        <p>Stars</p>
+        <h1>{locs[0].name}</h1>
+        {/* <img src= "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png" alt="location"/> */}
+        <img src="https://lh3.googleusercontent.com/p/AF1QipO9rpIMu80fHfYogE17u35ZC5r5WEhT47mHqSr7=s1360-w1360-h1020" alt="location"/>
+        <p>{locs[0].types[0]}, {locs[0].types[1]},{locs[0].types[2]} </p>
+        <p>Address: {locs[0].vicinity}</p>
+        <p>Rating: {locs[0].rating} / 5.0</p>
+        
     </div>
     );
 };
